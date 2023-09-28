14aa1d05cfa0400a5f3e1c7642b93dd3aa4dad96
fran885 <fspaltro@fi.uba.ar>
2023-09-28 19:06:24 -0300

    Test movimiento de basura a pila

--- java/src/test/java/KlondikeTest.java
+++ java/src/test/java/KlondikeTest.java
@@ -143,6 +143,28 @@ public class KlondikeTest {
     }
 
     @Test
+    public void testMoverBasuraAPila(){
+        // Arrange
+        Klondike klondike = new Klondike(Variante.KLONDIKE, true);
+        klondike.inicializarJuego();
+
+        // Act
+        klondike.moverMazoABasura();
+        // Queda un J diamantes (rojo) en la basura y Q treboles (negro) en la pila id 4
+
+        PilaDelTableau pilaDestino = klondike.obtenerPilaDelTableau(4);
+        boolean seMovio = klondike.moverBasuraAPila(pilaDestino);
+        Carta cartaMovida = pilaDestino.verUltima();
+
+        // Assert
+        assertTrue(seMovio);
+        assertEquals(5, klondike.obtenerPuntos());
+        assertEquals(Palo.DIAMANTES, cartaMovida.verPalo());
+        assertEquals(Valor.JOTA, cartaMovida.verValor());
+        assertTrue(klondike.basura.estaVacia());
+    }
+
+    @Test
     public void testMoverBasuraAMazo(){
         // Arrange
         Klondike klondike = new Klondike(Variante.KLONDIKE, true);
@@ -151,6 +173,8 @@ public class KlondikeTest {
         while (klondike.mazo.cantidadCartas() > 5) {
             klondike.moverMazoABasura();
         }
+
+        klondike.puntos = 50;
 
         // Assert
         assertEquals(5, klondike.mazo.cantidadCartas());
@@ -165,6 +189,7 @@ public class KlondikeTest {
         klondike.moverBasuraAMazo();
 
         // Assert
+        assertEquals(0, klondike.obtenerPuntos());
         assertTrue(klondike.basura.estaVacia());
         assertEquals(52, klondike.mazo.cantidadCartas());
 
@@ -179,7 +204,7 @@ public class KlondikeTest {
         // Act
         // Sacar la carta de la primera pila para que quede vacía
         klondike.pilasTableau.get(0).extraerUltima();
-        // La última carta de la segunda pila es J de corazones
+        // La última carta de la segunda pila es J de picas
         boolean seMovio = klondike.moverPilaAPila(klondike.pilasTableau.get(1), klondike.pilasTableau.get(0), 1);
 
         // Assert
@@ -194,7 +219,7 @@ public class KlondikeTest {
 
         // Act
         boolean seMovio = klondike.moverPilaAPila(klondike.pilasTableau.get(6), klondike.pilasTableau.get(0), 1);
-        // La primera carta de la primera pila es K corazones (rojo) y la última carta de la última pila es Q treboles (negro)
+        // La primera carta de la primera pila es K picas (negro) y la última carta de la última pila es Q diamantes (rojo)
 
         // Assert
         assertTrue(seMovio);
@@ -202,7 +227,7 @@ public class KlondikeTest {
 
         // Act
         seMovio = klondike.moverPilaAPila(klondike.pilasTableau.get(0), klondike.pilasTableau.get(6), 1);
-        // Ahora la última carta de la última pila es K tréboles (negro) y la de la primera es Q tréboles (negro)
+        // Ahora la última carta de la última pila es K diamantes (rojo) y la de la primera es Q diamantes (rojo)
 
         // Assert
         assertFalse(seMovio);
