414c2881a551b34a78140e1cb0fcd430624e94a1
fran885 <fspaltro@fi.uba.ar>
2023-11-05 13:37:34 -0300

    WIP ventana.fxml

--- java/src/main/java/ui/ClicCartaEvento.java
+++ java/src/main/java/ui/ClicCartaEvento.java
@@ -1,27 +1,26 @@
 package ui;
 
 import solitario.Klondike;
-import solitario.ListaDeCartas;
 import solitario.PilaDelTableau;
 
 public class ClicCartaEvento {
-    private boolean clic = false;
+    private boolean clicAnterior = false;
     private PilaDelTableau origen = null;
     private PilaDelTableau destino = null;
     private int indiceOrigen;
 
     public boolean huboClic(){
-        return clic;
+        return clicAnterior;
     }
     public boolean hacerClic(Klondike klondike, PilaDelTableau lugar, int indice){
-        if (clic) {
+        if (clicAnterior) {
             destino = lugar;
-            clic = false;
+            clicAnterior = false;
             lanzarEventoMover(klondike);
         } else {
             origen = lugar;
             indiceOrigen = indice;
-            clic = true;
+            clicAnterior = true;
             lanzarEventoSeleccion(klondike);
         }
     }
